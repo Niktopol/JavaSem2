a7319c26b87b555525e3827d4cdaf15bc6abcc1e
Kirill <kirill-22ru@yandex.ru>
2024-04-03 11:18:00 +0300

    Started tasks 13-15

---
+++ task13-15/src/main/java/com/example/demo/model/Game.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class Game {
+    private String name;
+    private Date creationDate;
+}
---
+++ task13-15/src/main/java/com/example/demo/model/GameAuthor.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+import java.util.Date;
+
+public class GameAuthor {
+    private String nickname;
+    private Date birthDate;
+}
